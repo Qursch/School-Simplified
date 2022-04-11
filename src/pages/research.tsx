6c933d69f11ab9d798420d0e5c93db43790984e2
Gui Guy <jedandeganwang1999@gmail.com>
2022-04-11 16:38:03 -0500

    Passed the opportunities array as a proper prop to the page

--- src/pages/research.tsx
+++ src/pages/research.tsx
@@ -1,12 +1,17 @@
 import { getResearchOpportunities } from "@api/notion";
 import UnderConstruction from "@components/underConstruction";
+import { Opportunity } from "types";
 
-export default function Research(): JSX.Element {
+export default function Research({
+	opportunities,
+}: {
+	opportunities: Opportunity[];
+}): JSX.Element {
+	console.log("opportunities: ", JSON.stringify(opportunities, null, 2));
 	return <UnderConstruction />;
 }
 
 export async function getStaticProps() {
 	const props = { opportunities: await getResearchOpportunities() };
-	console.log("props: ", props);
 	return { props, revalidate: 360 };
 }
