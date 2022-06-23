5f624fe7bd67a919418f170a11c0370663914fe2
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-06-23 14:08:43 -0400

    ts-jest is weird

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const nextJest = require("next/jest");
-
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
 const createJestConfig = nextJest({
 	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
 	dir: ".",
@@ -12,15 +13,9 @@ const customJestConfig = {
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: ["node_modules", "<rootDir>/"],
 	testEnvironment: "jest-environment-jsdom",
-
-	// Explicitly name each module with absolute paths attached since it's not taking any from tsconfig.json
-	moduleNameMapper: {
-		"@src/(.*)": "<rootDir>/src/$1",
-		"@pages/(.*)": "<rootDir>/src/pages/$1",
-		"@tests/(.*)": "<rootDir>/__tests__/$1",
-		"@components/(.*)": "<rootDir>/src/components/$1",
-		"@styles/(.*)": "<rootDir>/src/styles/$1",
-	},
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: "<rootDir>/src",
+	}),
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
